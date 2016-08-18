d214723018b655b01f34db713c996f23f0c8751b
mehrdadAkbari <akbarimehrdad87@yahoo.com>
2016-08-18 05:12:13 +0430

    re #3, class file, action file, asset file config
    Signed-off-by: mehrdadAkbari <akbarimehrdad87@yahoo.com>

---
+++ PrayerAction.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: M.Akbari
+ * Date: 8/17/2016
+ * Time: 10:30 PM
+ */
+namespace mehrdadakbari\prayerTime;
+
+use yii\base\Action;
+use Yii;
+
+class PrayerAction extends Action{
+    public $citiesList = [];
+    public function run(){
+        if(Yii::$app->request->post('setCityList')) {
+            $this->citiesList = Require('citiesList.php');
+            $province = Yii::$app->request->post('province');
+            $citiesOfProvince = $this->citiesList[$province];
+            foreach ($citiesOfProvince as $key => $val) {
+                $data[] = $key;
+            }
+            Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+            return $data;
+        }elseif (Yii::$app->request->post('calcutTime')){
+
+        }
+
+    }
+} 
---
+++ PrayerTimeAsset.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: M.Akbari
+ * Date: 8/11/2016
+ * Time: 11:29 PM
+ */
+namespace mehrdadakbari\prayerTime;
+
+use yii\web\AssetBundle;
+
+/**
+ * This declares the asset files required by Gii.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class PrayerTimeAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/mehrdadakbari/yii2-time-of-prayer/asset';
+    public $css = [
+        '/css/'
+    ];
+    public $js = [
+        '/js/',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+}
